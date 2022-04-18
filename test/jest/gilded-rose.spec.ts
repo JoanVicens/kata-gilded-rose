87e21a073be3b582c10c38e59d44caec49200d3e
Maria Soria <maria.it3@gmail.com>
2022-04-18 18:23:48 +0100

    add another snapshot test

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -7,7 +7,7 @@ describe('Gilded Rose', () => {
     expect(items[0].name).toBe('fixme');
   });
 
-  it('should do a snapshot', function () {
+  it('should do a snapshot for 2 days', function () {
     let items = [
       new Item("+5 Dexterity Vest", 10, 20), //
       new Item("Aged Brie", 2, 0), //
@@ -22,6 +22,35 @@ describe('Gilded Rose', () => {
 
     const gildedRose = new GildedRose(items);
 
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+
+    expect(items).toMatchSnapshot();
+  });
+
+  it('should do a snapshot for 10 days', function () {
+    let items = [
+      new Item("+5 Dexterity Vest", 10, 20), //
+      new Item("Aged Brie", 2, 0), //
+      new Item("Elixir of the Mongoose", 5, 7), //
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      // this conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6)];
+
+    const gildedRose = new GildedRose(items);
+
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
     gildedRose.updateQuality();
     gildedRose.updateQuality();
 
