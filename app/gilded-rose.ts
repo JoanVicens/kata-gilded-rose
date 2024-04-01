46f269a71d0e17050db89878672bce897488dc7c
joan vicens <joanvicens@proton.me>
2024-04-01 16:07:12 +0200

    simplify if statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,13 +10,11 @@ export class GildedRose {
   private updateItemQuality(item: Item): void {
     if (
       item.name != "Aged Brie" &&
-      item.name != "Backstage passes to a TAFKAL80ETC concert"
+      item.name != "Backstage passes to a TAFKAL80ETC concert" &&
+      item.name != "Sulfuras, Hand of Ragnaros" &&
+      item.quality > 0
     ) {
-      if (item.quality > 0) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
-        }
-      }
+      item.quality = item.quality - 1;
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
@@ -37,6 +35,7 @@ export class GildedRose {
     if (item.name != "Sulfuras, Hand of Ragnaros") {
       item.sellIn = item.sellIn - 1;
     }
+
     if (item.sellIn < 0) {
       if (item.name != "Aged Brie") {
         if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
